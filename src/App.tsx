3cd0de632837beb218c93824dd1a20d6a3b09e02
DEEN <studiousdoston@gmail.com>
2026-08-07 22:36:02 +0900

    feat: fetch data from server API

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,58 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+type Questions = {
+  question: string;
+  options: string[];
+  correctOption: number;
+  points: number;
+};
+type State = {
+  questions: Questions[];
+  status: string;
+};
+type Action = {
+  type: string;
+  payload?: Questions[];
+};
+
+const initialState = {
+  questions: [],
+  status: "loading", // loading, error, ready, active, finished
+};
+
+function reducer(state: State, action: Action): State {
+  console.log("state:", state, "action:", action);
+
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload!, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("Unknown error");
+  }
+} 
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { questions, status } = state;
+
+  useEffect(() => {
+    fetch("http://localhost:8010/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
-      <DateCounter />
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
