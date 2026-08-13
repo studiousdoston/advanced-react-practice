db844e373f9797d92ef164b3117ddcfe7abf46c2
DEEN <studiousdoston@gmail.com>
2026-08-13 23:01:55 +0900

    feat: learn how to keep the state in the URL using params&queries

--- 02-worldwise/src/App.tsx
+++ 02-worldwise/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Form, Route, Routes } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import Product from "./pages/Product";
@@ -9,12 +9,13 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountriesList from "./components/CountriesList";
-import { City } from "./libs/common";
+import City from "./components/City";
+import type { City as CityType } from "./libs/common";
 
 const BASE_URL = "http://localhost:3090";
 
 function App() {
-  const [cities, setCities] = useState<City[]>([]);
+  const [cities, setCities] = useState<CityType[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -48,11 +49,12 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountriesList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- 02-worldwise/src/components/CityItem.tsx
+++ 02-worldwise/src/components/CityItem.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styles from "../css/CityItem.module.css";
 import { City } from "../libs/common";
 
@@ -13,13 +14,18 @@ const formatDate = (date: string) =>
   }).format(new Date(date));
 
 export default function CityItem({ city }: Props) {
-  const { cityName, emoji, date } = city;
+  const { cityName, emoji, date, id, position } = city;
   return (
-    <li className={styles.cityItem}>
-      <span className={styles.emoji}>{emoji}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>({formatDate(date)})</time>
-      <button className={styles.deleteBtn}>&times;</button>
+    <li>
+      <Link
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+        className={styles.cityItem}
+      >
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>({formatDate(date)})</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
     </li>
   );
 }
