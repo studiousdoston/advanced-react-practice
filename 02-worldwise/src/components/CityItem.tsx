c23c39f073fd6380cbcdaba2e8f6e1228d7649f9
DEEN <studiousdoston@gmail.com>
2026-08-15 22:17:21 +0900

    feat: install and costmize leaflet package for Map  compononent

--- 02-worldwise/src/components/CityItem.tsx
+++ 02-worldwise/src/components/CityItem.tsx
@@ -21,7 +21,7 @@ export default function CityItem({ city }: Props) {
     <li>
       <Link
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
-        className={`${styles.cityItem} ${id === currentCity.id ? styles["cityItem--active"] : ""}`}
+        className={`${styles.cityItem} ${id === currentCity?.id ? styles["cityItem--active"] : ""}`}
       >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
--- 02-worldwise/src/components/Map.tsx
+++ 02-worldwise/src/components/Map.tsx
@@ -1,21 +1,34 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "../css/Map.module.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useState } from "react";
 
 export default function Map() {
-  const [searchParams, setSearchParams] = useSearchParams();
   const navigate = useNavigate();
+  const [mapPosition, setMapPosition] = useState<[number, number]>([40, 0]);
+
+  const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <h1>
-        Position: {lat}, {lng}
-      </h1>
-      <button onClick={() => setSearchParams({ lat: "23", lng: "50" })}>
-        Change position
-      </button>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        className={styles.map}
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
