49d5791fa945fee5e319ecb86c8e732290847d44
DEEN <studiousdoston@gmail.com>
2026-08-26 21:27:29 +0900

    feat: Learn Redux initial setup

---
+++ 04-redux-intro/src/BalanceDisplay.tsx
@@ -0,0 +1,12 @@
+function formatCurrency(value: string | number | bigint) {
+  return new Intl.NumberFormat("en", {
+    style: "currency",
+    currency: "USD",
+  }).format(typeof value === "string" ? Number(value) : value);
+}
+
+function BalanceDisplay() {
+  return <div className="balance">{formatCurrency(123456)}</div>;
+}
+
+export default BalanceDisplay;
