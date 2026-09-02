46db9a89d70d65b4a8936bd8e6722bf027ecacb8
DEEN <studiousdoston@gmail.com>
2026-09-03 07:04:33 +0900

    feat: develop AppLayout as the parent component for the homepage

---
+++ 05-fast-react-pizza/src/ui/AppLayout.tsx
@@ -0,0 +1,16 @@
+import CartOverview from "@/features/cart/CartOverview";
+import Header from "./Header";
+import { Outlet } from "react-router-dom";
+
+export default function AppLayout() {
+  return (
+    <div>
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+      <CartOverview />
+    </div>
+  );
+}
