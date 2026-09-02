261e8da49d9567345d687fb41d9db3c0c6db3ec1
DEEN <studiousdoston@gmail.com>
2026-09-03 06:29:13 +0900

    feat: integrate features components and utils functions and some other components

---
+++ 05-fast-react-pizza/src/services/apiGeocoding.ts
@@ -0,0 +1,13 @@
+type AddressProps = {
+  latitude: string;
+  longitude: string;
+};
+export async function getAddress({ latitude, longitude }: AddressProps) {
+  const res = await fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`,
+  );
+  if (!res.ok) throw Error("Failed getting address");
+
+  const data = await res.json();
+  return data;
+}
---
+++ 05-fast-react-pizza/src/types/cart.types.ts
@@ -0,0 +1,7 @@
+export interface Cart {
+  pizzaId: number;
+  name: string;
+  quantity: number;
+  unitPrice: number;
+  totalPrice: number;
+}
