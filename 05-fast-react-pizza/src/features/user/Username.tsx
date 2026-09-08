7f9623da195a3be3a23c021a147451aa5a427496
DEEN <studiousdoston@gmail.com>
2026-09-08 15:09:49 +0900

    feat: integrate redux to the project

--- 05-fast-react-pizza/src/features/user/Username.tsx
+++ 05-fast-react-pizza/src/features/user/Username.tsx
@@ -1,3 +1,11 @@
+import { RootState } from "@/store";
+import { useSelector } from "react-redux";
+
 export default function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Deen</div>;
+  const userName = useSelector((state: RootState) => state.user.username);
+
+  if (!userName) return null;
+  return (
+    <div className="hidden text-sm font-semibold md:block">{userName}</div>
+  );
 }
--- 05-fast-react-pizza/src/features/user/userSlice.ts
+++ 05-fast-react-pizza/src/features/user/userSlice.ts
@@ -1,3 +1,4 @@
+/*
 function getPosition() {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -19,3 +20,26 @@ async function fetchAddress() {
   // 3) Then we return an object with the data that we are interested in
   return { position, address };
 }
+*/
+
+import { createSlice } from "@reduxjs/toolkit";
+
+export interface State {
+  username: string;
+}
+
+const initialState: State = { username: "Dostonjon" };
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: initialState,
+  reducers: {
+    updateName(state, action) {
+      state.username = action.payload;
+    },
+  },
+});
+
+export const { updateName } = userSlice.actions;
+
+export default userSlice.reducer;
