b7abd3b6dada10ee19e2eff4b93c805e3dc69ed0
DEEN <studiousdoston@gmail.com>
2026-09-08 14:46:15 +0900

    feat: style components using Tailwind Css Part III

--- 05-fast-react-pizza/src/features/order/CreateOrder.tsx
+++ 05-fast-react-pizza/src/features/order/CreateOrder.tsx
@@ -66,7 +66,11 @@ function CreateOrder() {
               name="phone"
               required
             />
-            {formErrors?.phone && <p>{formErrors.phone}</p>}
+            {formErrors?.phone && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
         </div>
 
@@ -81,7 +85,7 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex items-center gap-5">
           <input
             className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
@@ -90,7 +94,9 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority" className="font-medium">
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
