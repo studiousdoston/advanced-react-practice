a2bc0c4df0e0990415794763b99e203e65c9edaf
DEEN <studiousdoston@gmail.com>
2026-09-09 22:37:22 +0900

    feat: style AppLayout component and change all the files from jsx to tsx

--- 06-the-wild-oasis/src/App.tsx
+++ 06-the-wild-oasis/src/App.tsx
@@ -1,53 +1,35 @@
-import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyle";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
+import Bookings from "./pages/Bookings";
+import AppLayout from "./ui/AppLayout";
 
-const StyledApp = styled.div`
-  padding: 20px;
-`;
-
-function App() {
+export default function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <Row type="vertical">
-          <Row type="horizontal">
-            <Heading as="h1">The Wild Oasis</Heading>
-
-            <div>
-              <Heading as="h2">Check in and out</Heading>
-              <Button
-                variation="primary"
-                size="medium"
-                onClick={() => alert("Checking in!")}
-              >
-                Check in{" "}
-              </Button>
-              <Button
-                variation="secondary"
-                size="small"
-                onClick={() => alert("Checking out!")}
-              >
-                Check out{" "}
-              </Button>
-            </div>
-          </Row>
-
-          <Row type="vertical">
-            <Heading as="h3">Form</Heading>
-            <form>
-              <Input type="number" placeholder="Number of Guests" />
-              <Input type="number" placeholder="Number of Guests" />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to={"dashboard"} />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
-
-export default App;
