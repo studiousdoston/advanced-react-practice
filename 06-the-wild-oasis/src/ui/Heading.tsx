87c262d3d7228b2f9a9922c6aa1e2c2caea9568e
DEEN <studiousdoston@gmail.com>
2026-09-17 20:56:17 +0900

    feat: develop basic login page

--- 06-the-wild-oasis/src/ui/Heading.tsx
+++ 06-the-wild-oasis/src/ui/Heading.tsx
@@ -2,7 +2,7 @@
 import styled, { css } from "styled-components";
 
 export interface HeadingProps {
-  as?: "h1" | "h2" | "h3";
+  as?: "h1" | "h2" | "h3" | "h4";
 }
 
 const Heading = styled.h1<HeadingProps>`
@@ -25,6 +25,13 @@ const Heading = styled.h1<HeadingProps>`
     css`
       font-size: 2rem;
       font-weight: 500;
+    `}
+  ${(props) =>
+    props.as === "h4" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+      text-align: center;
     `}
 
   line-height: 1.4
